208936b0aa75c72d3aa8dac0a2cc5463d72d3afb
bew3617 <86280226+bew3617@users.noreply.github.com>
2022-07-12 19:50:56 +0700

    Create Exercise4_Pratchaya_S.py

---
+++ Exercise4_Pratchaya_S.py
@@ -0,0 +1,12 @@
+#Variables
+Fe = 84
+Gb= 55.5
+ICS = 90
+CP = 95.6
+
+#Print
+print("--- Your Score ---")
+print("Foundation English :", Fe)
+print("General Business :", Gb)
+print("Introduction to Computer Systems :", ICS)
+print("Computer Programming :", CP)
