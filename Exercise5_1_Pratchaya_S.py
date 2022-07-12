9f079f52d41abdc9a8b571f4252866c081eca3fe
bew3617 <86280226+bew3617@users.noreply.github.com>
2022-07-12 20:09:44 +0700

    Create Exercise5_1_Pratchaya_S.py

---
+++ Exercise5_1_Pratchaya_S.py
@@ -0,0 +1,12 @@
+num1 = int(input("Insert First Number: "))
+num2 = int(input("Insert Second Number: "))
+plus = num1+num2
+minus = num1-num2
+multiply = num1 * num2
+divide = num1/num2
+
+print("This is Calculate program")
+print(num1, "+", num2, "=", plus)
+print(num1, "-", num2, "=", minus)
+print(num1, "*", num2, "=", multiply)
+print(num1, "/", num2, "=", int(divide))
